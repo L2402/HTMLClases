5685fa948745c2937f3302de0e675c1f42dc1502
L2402 <jeremyrevi94@gmail.com>
2025-07-16 21:42:47 -0500

    Semana 13, actividad en clase

---
+++ SegundoParcial/semana13ActividadClase/semana13/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import IncidenciaView from './view/IncidenciaView.vue';
+import vistaComerciante from './view/vistaComerciante.vue';
+import vistaNotificacion from './view/vistaNotificacion.vue';
+</script>
+
+<template>
+
+  <!-- hecho por Neysser Delgado Meza -->
+  <IncidenciaView>
+  </IncidenciaView>
+
+  <!-- hecho por Leo Holguin Chavez -->
+  <vistaComerciante>
+  </vistaComerciante>
+
+  <!-- Fue hecho por: Reyes Jeremy -->
+  <vistaNotificacion>
+
+  </vistaNotificacion>
+
+
+</template>
+
+<style scoped>
+
+</style>
